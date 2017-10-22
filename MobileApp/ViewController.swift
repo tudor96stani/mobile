c137b41611668e495b0c196a44dab92e44e25a8f
Tudor Stanila <tudorstanila@gmail.com>
2017-10-22 13:50:51 +0300

    Initial Commit

---
+++ MobileApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileApp
+//
+//  Created by Tudor Stanila on 22/10/2017.
+//  Copyright © 2017 Tudor Stanila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
